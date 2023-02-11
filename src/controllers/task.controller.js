62ca89ccf3d8f853932219b0ac62c13944ccfd2b
webdev1903 <vantureprakash1903@gmail.com>
2023-02-12 03:34:48 +0530

    resolved a bug

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -5,28 +5,38 @@ const router = express.Router();
 const Task = require("../models/task.model");
 
 const authenticate = require("../middlewares/authenticate");
-const current = new Date()
-  .toString()
-  .split(" ")
-  .filter((e, i) => i <= 3)
-  .join(" ")
-  .trim();
 
 router.post("/", authenticate, async (req, res) => {
   try {
+    const current = new Date()
+      .toString()
+      .split(" ")
+      .filter((e, i) => i <= 3)
+      .join(" ")
+      .trim();
     const tasks = await Task.find({ userId: req.user._id, time: current });
     if (tasks.length == 5) {
       return res
         .status(405)
         .send({ message: "You can only add 5 tasks in a day" });
     }
-    const task = await Task.create({ ...req.body, userId: req.user._id });
+    const task = await Task.create({
+      ...req.body,
+      time: current,
+      userId: req.user._id,
+    });
     return res.status(201).send({ task, message: "task successfully created" });
   } catch (error) {}
 });
 
 router.get("/", authenticate, async (req, res) => {
   try {
+    const current = new Date()
+      .toString()
+      .split(" ")
+      .filter((e, i) => i <= 3)
+      .join(" ")
+      .trim();
     const tasks = await Task.find({ userId: req.user._id, time: current });
     return res.status(200).send(tasks);
   } catch (error) {
